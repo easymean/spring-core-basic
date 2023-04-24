34ec0f0a97287b33af4045818459e040937d9a54
easymean <m2162003@naver.com>
2023-04-25 01:30:37 +0900

    스프링 컨테이너는 객체를 싱글턴으로 관리한다.

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -11,7 +13,7 @@ public class SingletonTest {
 
     @Test
     @DisplayName("스프링없는 순수한 DI 컨테이너")
-    void pureContainer(){
+    void pureContainer() {
         AppConfig appConfig = new AppConfig();
 
         // 1. 조회: 호출할 때 마다 객체를 생성
@@ -24,17 +26,25 @@ public class SingletonTest {
         assertThat(memberService1).isNotSameAs(memberService2);
     }
 
-//    public static void main(String[] args) {
-//        SingletonService singletonService = new SingletonService(); // private으로 선언해서 호출 불가
-//    }
-
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
-    void singletonServiceTest(){
+    void singletonServiceTest() {
         SingletonService singletonService1 = SingletonService.getInstance();
         SingletonService singletonService2 = SingletonService.getInstance();
 
         assertThat(singletonService1).isSameAs(singletonService2); // 인스턴스 비교는 isSameAs
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글턴")
+    void springContainer() {
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른가: 스프링을 사용하면 같다.
+        assertThat(memberService1).isSameAs(memberService2);
     }
 
 
