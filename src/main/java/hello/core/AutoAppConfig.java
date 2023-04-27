6d7ca4ee3fd051de1eca655931982b0643bc1daf
easymean <m2162003@naver.com>
2023-04-27 23:01:20 +0900

    컴포넌트 스캔 요약

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -8,7 +11,7 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
 //        basePackages = "hello.core.member", // 시작점을 지정. 없으면 모든 자바 클래스를 다 조회한다.
 //        basePackageClasses = AutoAppConfig.class,
-        // base를 지정하지 않는다면 여기가 시작점
+// base를 지정하지 않는다면 여기가 시작점
                 excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
